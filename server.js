ab15db0ae6086529a492f2ce946285d4e9cf38db
Murilo Weishaupt <muriloweishaupt@gmail.com>
2024-07-19 14:26:35 -0300

    Primeiro commit API Carros

---
+++ server.js
@@ -0,0 +1,10 @@
+import app from './src/app.js'
+
+
+const PORT = 3000;
+
+ //Escutar a porta 3000
+ app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+})
+
