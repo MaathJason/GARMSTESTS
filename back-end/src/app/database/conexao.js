1b221daf5cb005e6cfe4211fdc64086674bfae8e
Murilo Weishaupt <muriloweishaupt@gmail.com>
2024-08-07 16:46:34 -0300

    environment

--- back-end/src/app/database/conexao.js
+++ back-end/src/app/database/conexao.js
@@ -1,13 +1,32 @@
-import mysql from "mysql"
+// import mysql from "mysql"
+
+// const conexao = mysql.createConnection({
+//     host: 'database-1.cle2qmq2knj6.us-east-2.rds.amazonaws.com',
+//     port: '3306',
+//     user: 'admin',
+//     password: 'garmsninguemtapuro',
+//     database: 'database-1'
+// })
+
+// conexao.connect()
+
+// export default conexao
+
+
+// db.js
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const conexao = mysql.createConnection({
-    host: 'database-1.cle2qmq2knj6.us-east-2.rds.amazonaws.com',
-    port: '3306',
-    user: 'admin',
-    password: 'garmsninguemtapuro',
-    database: 'database-1'
-})
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-conexao.connect()
+conexao.connect();
 
-export default conexao
+export default conexao;
