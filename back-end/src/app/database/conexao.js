90106d07110c4d33781abc2d1362e97618d58f8a
Murilo Weishaupt <muriloweishaupt@gmail.com>
2024-08-07 00:10:27 -0300

    Adicionado banco de dados na nuvem

--- back-end/src/app/database/conexao.js
+++ back-end/src/app/database/conexao.js
@@ -1,11 +1,11 @@
 import mysql from "mysql"
 
 const conexao = mysql.createConnection({
-    host: 'localhost',
+    host: 'database-1.cle2qmq2knj6.us-east-2.rds.amazonaws.com',
     port: '3306',
-    user: 'root',
-    password: 'root',
-    database: 'dbcars'
+    user: 'admin',
+    password: 'garmsninguemtapuro',
+    database: 'database-1'
 })
 
 conexao.connect()
