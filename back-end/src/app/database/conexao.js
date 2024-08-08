d7d8fc7ff5f79ccd0f8ddaff4a7c2e4cf9ee5b07
MWeishaupt <113941985+MuriloWeishaupt@users.noreply.github.com>
2024-08-08 16:48:45 -0300

    Update conexao.js

--- back-end/src/app/database/conexao.js
+++ back-end/src/app/database/conexao.js
@@ -1,23 +1,15 @@
 import mysql from 'mysql';
-import dotenv from 'dotenv';
-dotenv.config();
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
+
 
 const conexao = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
+    host: 'database-1.cle2qmq2knj6.us-east-2.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: 'garmsninguemtapuro',
+    database: 'database-1'
+})
 
 conexao.connect();
 
 export default conexao;
-
