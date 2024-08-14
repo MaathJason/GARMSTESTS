ecd5f67e87ae59974a232d188252deb7a769697f
Bowie <marketing@letscode.com.br>
2024-08-14 19:52:52 -0300

    dbtest

--- back-end/src/app/database/conexao.js
+++ back-end/src/app/database/conexao.js
@@ -6,8 +6,8 @@ const conexao = mysql.createConnection({
     host: 'database-1.cle2qmq2knj6.us-east-2.rds.amazonaws.com',
     port: '3306',
     user: 'admin',
-    password: 'garmsninguemtapuro',
-    database: 'database-1'
+    password: 'dbgarms.test',
+    database: 'database_1'
 })
 
 conexao.connect();
