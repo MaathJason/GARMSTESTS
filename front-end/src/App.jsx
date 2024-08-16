7657f8492f54a518c6f9e6310218c0cc24be4b15
Maath.J <150763278+MaathJason@users.noreply.github.com>
2024-08-16 15:53:03 -0300

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 const api = axios.create({
-  baseURL: 'http://3.143.237.188:3000',
+  baseURL: 'http://localhost:3000',
 });
 
 function App() {
