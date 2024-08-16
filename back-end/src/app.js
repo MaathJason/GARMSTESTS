cb9df30c452c5efd42bd52bae6c53f70135d2dfb
maath.j <mateua12392@gmail.com>
2024-08-16 11:41:06 -0300

    nopath

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,9 +4,6 @@ import cors from 'cors'
 import dotenv from 'dotenv';
 dotenv.config();
 
-const path = require('path')
-
-app.use(express.static(path.join(__dirname, '/front-end/build')));
 
 const app = express();
 
