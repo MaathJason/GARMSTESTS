e2720d0278a92c9892655348dc0bb566c4018a2f
maath.j <mateua12392@gmail.com>
2024-08-16 11:28:26 -0300

    commit backend

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -7,5 +7,12 @@ export default defineConfig({
   server:{
     host: true,
     port: 80,
+    Proxy:{
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   },
 })
