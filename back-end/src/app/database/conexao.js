ca7293c7648db04aab5b35afa9c35818197d3d09
maathjason <mateua12392@gmail.com>
2024-08-20 13:54:32 -0300

    testdb

--- back-end/src/app/database/conexao.js
+++ back-end/src/app/database/conexao.js
@@ -9,7 +9,7 @@ const conexao = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-});
+}); 
 
 conexao.connect((err) => {
     if (err) {
