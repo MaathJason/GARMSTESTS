598e6e68b65553452a0fb32baacea004ca07b45f
maath.j <mateua12392@gmail.com>
2024-08-20 11:26:20 -0300

    bdtest2

--- back-end/src/app/database/conexao.js
+++ back-end/src/app/database/conexao.js
@@ -1,19 +1,22 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 import mysql from 'mysql2';
 
-
 const conexao = mysql.createConnection({
-    //host: 'database-1.cle2qmq2knj6.us-east-2.rds.amazonaws.com',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-})
+});
 
-conexao.connect();
+conexao.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar com o banco de dados:', err);
+        return;
+    }
+    console.log('Conectado ao banco de dados!');
+});
 
 export default conexao;
