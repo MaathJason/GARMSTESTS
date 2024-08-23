48a59ba7fe579b41418a9cea2e96a2314ad7dec9
maath.j <mateua12392@gmail.com>
2024-08-23 09:25:57 -0300

    mochatest4

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,35 +2,48 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import CarroController from './app/controllers/CarroController.js';
-import cors from 'cors'
-
+import cors from 'cors';
+import client from 'prom-client';
 
 const app = express();
+const register = client.register;
 
+// Define uma métrica de exemplo
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'status_code'],
+});
 
-//Indicar para o express ler body com JSON e usar o CORS (qualquer front-end pode acessar o server)
-app.use(express.json())
-app.use(cors())
+// Middleware para contar requisições
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({ method: req.method, status_code: res.statusCode });
+  });
+  next();
+});
 
-//CREATE (C)
-app.post('/carros', CarroController.store)
+// Adiciona o endpoint /metrics
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
+// Indicar para o express ler body com JSON e usar o CORS
+app.use(express.json());
+app.use(cors());
 
-//READ (R)
-app.get('/carros', CarroController.index)
+// CREATE (C)
+app.post('/carros', CarroController.store);
 
-app.get('/carros/:id', CarroController.show)
+// READ (R)
+app.get('/carros', CarroController.index);
+app.get('/carros/:id', CarroController.show);
 
+// UPDATE (U)
+app.put('/carros/:id', CarroController.update);
 
-//UPDATE (U)
-app.put('/carros/:id', CarroController.update)
+// DELETE (D)
+app.delete('/carros/:id', CarroController.delete);
 
-
-//DELETE (D)
-app.delete('/carros/:id', CarroController.delete)
-
-
-
-
-
-export default app
+export default app;
