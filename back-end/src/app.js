407216fa988e0d9221a8fb7fd6632a112430b11c
maathjason <mateua12392@gmail.com>
2024-09-26 14:44:19 -0300

    Exclusão metrics

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -8,27 +8,6 @@ import client from 'prom-client';
 const app = express();
 const register = client.register;
 
-// Define uma métrica de exemplo
-const httpRequestCounter = new client.Counter({
-  name: 'http_requests_total',
-  help: 'Total number of HTTP requests',
-  labelNames: ['method', 'status_code'],
-});
-
-// Middleware para contar requisições
-app.use((req, res, next) => {
-  res.on('finish', () => {
-    httpRequestCounter.inc({ method: req.method, status_code: res.statusCode });
-  });
-  next();
-});
-
-// Adiciona o endpoint /metrics
-app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', register.contentType);
-  res.end(await register.metrics());
-});
-
 // Indicar para o express ler body com JSON e usar o CORS
 app.use(express.json());
 app.use(cors({
@@ -50,4 +29,3 @@ app.put('/carros/:id', CarroController.update);
 app.delete('/carros/:id', CarroController.delete);
 
 export default app;
-
