942f3c070d00140164c25412b23ff3e076b4f298
maath.j <mateua12392@gmail.com>
2024-09-30 23:17:08 -0300

    erro certbot

--- front-end/start.sh
+++ front-end/start.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
 # Obter certificados com Certbot
-certbot --nginx -d garmsops.com.br -d www.garmsops.com.br --non-interactive --agree-tos --email garmsltda@gmail.com
+certbot certonly --nginx -d garmsops.com.br -d www.garmsops.com.br --non-interactive --agree-tos --email garmsltda@gmail.com --debug
+
+
+# Verifique se os certificados foram gerados
+if [ -f /etc/letsencrypt/live/garmsops.com.br/fullchain.pem ]; then
+    echo "Certificados gerados com sucesso."
+else
+    echo "Falha ao gerar certificados."
+    exit 1
+fi
 
 # Iniciar o Nginx
 nginx -g "daemon off;"
