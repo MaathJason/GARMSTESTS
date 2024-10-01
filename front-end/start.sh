44eb9663be5873730becbb7c7a2b45ddc42bf078
maath.j <mateua12392@gmail.com>
2024-09-30 23:31:45 -0300

    Teste .sh

--- front-end/start.sh
+++ front-end/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Obter certificados com Certbot
-certbot certonly --nginx -d garmsops.com.br -d www.garmsops.com.br --non-interactive --agree-tos --email seu_email@example.com
+sudo certbot certonly --nginx -d garmsops.com.br -d www.garmsops.com.br --non-interactive --agree-tos --email seu_email@example.com
 
 # Verifique se os certificados foram gerados
 if [ -f /etc/letsencrypt/live/garmsops.com.br/fullchain.pem ]; then
